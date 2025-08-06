e79ab0812fc43b558a21a55f2c8843d4661c6503
tkogiah <tkogiah@gmail.com>
2025-08-06 16:21:34 -0700

    added UI but mostly practicing git branching

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ const computerSelection = function getComputerChoice(){
 const humanSelection = () =>  prompt("choose rock, paper, or scissors")
 
 function playRound(humanChoice, computerChoice) {
+    display.innerText = ''
     let human = humanChoice
     let computer = computerChoice
     console.log('player chooses ' + human)
     console.log( 'computer chooses ' + computer)
     //returns if there is a tie
     if (human == computer) {
-      console.log("it was a tie try again")
+      display.innerText = "It was a tie try again"
     } 
     //returns if player wins
     if (human == 'paper' && computer == 'rock' ||
@@ -39,18 +40,38 @@ function playRound(humanChoice, computerChoice) {
         }
     //checks to see if game is over with computer victory
     if(computerScore === 5) {
-        console.log(`computer wins with ${computerScore} points`)
+        score.innerText = `Computer wins with ${computerScore} points`
+        display.appendChild(score)
         computerScore = 0
         humanScore = 0
         return
     }
     //checks to see if game is over with player victory
     if(humanScore === 5) {
-        console.log(`player wins with ${humanScore} points`)
+        score.innerText = `Player wins with ${humanScore} points`
+        display.appendChild(score)
         computerScore = 0
         humanScore = 0
         return
     }
-    console.log("human has " + humanScore + " points")
-    console.log("computer has " + computerScore + " points")
+    score.innerText = `Human has a score of ${humanScore} \n Computer has a score of ${computerScore}`
+    display.appendChild(score)
+    
 }
+
+const playRoundRock = () => playRound("rock", computerSelection());
+const playRoundPaper = () => playRound("paper", computerSelection());
+const playRoundScissors = () => playRound("scissors", computerSelection());
+
+const rock = document.querySelector('.rock-button').addEventListener("click", e => {
+    playRoundRock()
+})
+const paper = document.querySelector('.paper-button').addEventListener("click", e => {
+    playRoundPaper()
+})
+const scissors = document.querySelector('.scissors-button').addEventListener("click", e => {
+    playRoundScissors()
+})
+
+const display = document.querySelector('.console-display')
+const score = document.createElement('div')
